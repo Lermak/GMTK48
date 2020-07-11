c88797280fb05f26f61d76614a94ebb1cc68a3a5
Mettra <Mettra@users.noreply.github.com>
2020-07-11 16:24:41 -0700

    fix crash

--- Game/GameObjects/WireCoupling.lua
+++ Game/GameObjects/WireCoupling.lua
@@ -66,7 +66,9 @@ function WireCoupling:onUpdate(dt)
               Cursor.wireEnd = nil
               flag = true
 
-              ConnectNode(self.wireEnds[1].myNode.nodeIdx, self.wireEnds[2].myNode.nodeIdx)
+              if self.wireEnds[1].myNode and self.wireEnds[2].myNode then
+                ConnectNode(self.wireEnds[1].myNode.nodeIdx, self.wireEnds[2].myNode.nodeIdx)
+              end
             elseif Cursor.wireEnd == nil and y.isConnected == true and y.wireEnd == w then
               DisconnectNode(w.myNode.nodeIdx)
 
